84d04d95201b03ad15dacebf28480f9b0072ab63
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-02-23 16:25:23 +0700

    completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1, Rect *R2){
+	R1->w = min(R2->x+R2->w,R1->x+R1->w) - max(R1->x,R2->x);
+	R2->h = min(R2->y,R1->y) - max(R2->y-R2->h,R1->y-R1->h);
+	if(R1->w > 0 && R2->h > 0){
+		return R1->w*R2->h;
+	}
+	else return 0;
 }
